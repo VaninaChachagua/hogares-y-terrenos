844e92fc7b58d9caa7caabc8b7cf318969a92946
Vanina <chachagua.vanina@gmail.com>
2020-09-07 20:40:03 -0300

    Despliegue 2.0

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,6 +8,8 @@ app.use(require('./upload'));
 app.use(require('./imagenes'));
 
 //Ruta genérica
-// express.Router().get('*', (req, res) => { res.redirect('/'); });
+express.Router().get('/login', (req, res) => { res.redirect('/login'); });
+express.Router().get('/home', (req, res) => { res.redirect('/home'); });
+express.Router().get('*', (req, res) => { res.redirect('/'); });
 
 module.exports = app;
